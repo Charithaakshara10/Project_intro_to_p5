1428de67d3e69bbd4fd2b8f67372c49eed8d3eb3
Charithaakshara10 <92734048+Charithaakshara10@users.noreply.github.com>
2023-08-19 16:02:51 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 480)
+    canvas.position(110, 250);
+}
+
+function draw()
+{
+    fil(o, 128, 0);
+    stroke(o, 128, 0);
+    rect(450, 50, 70);
+}
+
+
+
